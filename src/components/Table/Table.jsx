2422167f5c5758723670d9ea5b1efc00265ce97b
kennethashley <ken.u.diggitt@gmail.com>
2018-12-03 12:50:23 -0500

    Add Table component

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const StyledTable = styled.table`
+  border-collapse: collapse;
+  font-size: 0.9em;
+  text-align: left;
+  width: 100%;
+
+  th, td {
+    padding: 12px;
+  }
+
+  th {
+    background-color: #F5F6F8;
+    font-family: 'Rubik';
+    padding: 12px;
+
+    &:not(:last-child) {
+      border-right: 2px solid #fff;
+    }
+  }
+`;
 
 class Table extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = { };
   }
   render() { 
+    const { columns, dataSource } = this.props;
+
     return (
+      <StyledTable>
+        <thead>
+          <tr>
+            { columns.map(column => (
+              <th key={column.key}>{ column.title }</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          { dataSource.map(source => this.props.render(source)) }
+        </tbody>
+      </StyledTable>
     );
   }
 }
--- src/scenes/OpenOffers.jsx
+++ src/scenes/OpenOffers.jsx
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
 
 import Container from '../components/Container';
+import Table from '../components/Table';
+
+const dataSource = [{
+  key: '1',
+  name: 'Mike',
+  age: 32,
+  address: '10 Downing Street'
+}, {
+  key: '2',
+  name: 'John',
+  age: 42,
+  address: '10 Downing Street'
+}];
+
+const columns = [{
+  title: 'Name',
+  dataIndex: 'name',
+  key: 'name',
+}, {
+  title: 'Age',
+  dataIndex: 'age',
+  key: 'age',
+}, {
+  title: 'Address',
+  dataIndex: 'address',
+  key: 'address',
+}];
 
 class OpenOffers extends Component {
   render() {
     return (
       <Container>
-        <h1>Open Offers</h1>
+        <Table
+          columns={columns}
+          dataSource={dataSource}
+          render={source => (
+            <tr key={source.key}>
+              <td>{ source.name }</td>
+              <td>{ source.age }</td>
+              <td>{ source.address }</td>
+            </tr>
+          )} />
       </Container>
-    )
+    );
   }
 }
  
